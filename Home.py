36584a16eb9716156b2e75ce85ca329c1faad973
Brian McGrail <brianmcgrail@macbookpro.mynetworksettings.com>
2026-01-14 17:28:20 -0500

    Update models and data

--- Home.py
+++ Home.py
@@ -1,30 +1,6 @@
 
 import streamlit as st
 
-# Toggle
-theme_choice = st.toggle("🌙 Dark mode", value=True)
-
-if theme_choice:
-    # DARK
-    st.markdown("""
-        <style>
-        .stApp {
-            background-color: #0E1117;
-            color: white;
-        }
-        </style>
-    """, unsafe_allow_html=True)
-else:
-    # LIGHT
-    st.markdown("""
-        <style>
-        .stApp {
-            background-color: white;
-            color: black;
-        }
-        </style>
-    """, unsafe_allow_html=True)
-
 st.set_page_config(page_title="RouteTree Labs Predictive Models", layout="wide")
 
 st.title("RouteTree Labs Prediction Models")
