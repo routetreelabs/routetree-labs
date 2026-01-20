f60cf860e0200fde487b19904d45913f3062e80f
Brian McGrail <brianmcgrail@macbookpro.mynetworksettings.com>
2026-01-20 16:43:46 -0500

    Update models and data

--- Home.py
+++ Home.py
@@ -7,9 +7,11 @@ st.title("RouteTree Labs Sports Prediction Models")
 st.caption("RouteTree Labs delivers NFL and NBA predictions powered by machine learning, helping sharp bettors and sports fans make smarter moves backed by real data.")
 st.write("""Welcome to the Lab. Choose a model from the left sidebar:
 
-- **2026 NBA Points and Spread Predictor** (Ridge Regression)
+- **2026 NBA Points Predictor** (Ridge Regression)
+- **2026 NBA Spreads Predictor** (Logistic Regression)
+- **2025 NFL Playoffs Predictions** (Logistic Regression, Linear Regression)
 - **2025 NFL Moneyline Predictions** (Logistic Regression)
-- **2025 NFL Over/Under Predictions** (K-Nearest Neighbors)
 - **2025 NFL Points Predictor** (Linear Regression)
 - **2025 NFL Against the Spread Predictor** (Logistic Regression)
+- **2025 NFL Over/Under Predictions** (K-Nearest Neighbors)
 """)
